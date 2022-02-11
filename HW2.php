6633558ebde7f1d1ea963549d30c7f2b1f8b3859
Roman Ponomarenko <roman.ponomarenko21@gmail.com>
2022-02-11 22:01:22 +0200

    My HW 1-3

---
+++ HW2.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = rand(0,100);
+echo "$a \n";
+
+if ($a % 2) {
+    echo "Odd";
+} else {
+    echo "Even";
+}
---
+++ HW3.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = rand(0,50);
+echo "$a \n";
+
+switch ($a) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        echo "Odd";
+        break;
+    case 10:
+    case 20:
+    case 30:
+    case 40:
+    case 50:
+        echo "Even";
+        break;
+    default:
+        echo "Ooops";
+}
